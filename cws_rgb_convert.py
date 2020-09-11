bd44e686ba07916d859b3d0436d223c8dc96ab86
PhiliNBG <philinbg@gmail.com>
2020-09-11 11:53:09 +0200

    Update cws_rgb_convert.py

--- cws_rgb_convert.py
+++ cws_rgb_convert.py
@@ -7,6 +7,8 @@ import sys
 import os
 import re
 
+from PIL import Image
+
 def yes_or_no(question):
     while "the answer is invalid":
         reply = str(input(question+' (y/n): ')).lower().strip()
@@ -26,11 +28,11 @@ tempdir = infile+ "_temp"
 changename = infile.split(".")[0]
 print(changename)
 
-# Entpacken
+# Unzip CWS-File
 with zipfile.ZipFile(infile, 'r') as zip_ref:
     zip_ref.extractall(tempdir)
 
-# Preview entfernen und Namen ändern
+# Remove Preview-Image and change filenames
 for f in os.listdir(tempdir):
     #print(f)
     filestring = str(f)
@@ -54,7 +56,32 @@ for f in os.listdir(tempdir):
         	print("Error renaming file")        
         #os.rename("./"+tempdir+"/"+f,"./"+tempdir+"/"+ changename + ".gcode")
 
-print("Iteration 1 END")
+
+def rgb_convert(file):
+    try:
+	    inImg = Image.open(file)
+    except:
+	    print( "ERROR: Could not open " + file + " for reading.")
+	    exit(1)
+    inType = inImg.mode
+    if inType == "LA" or inType == "L":
+        inImg = inImg.convert("L")
+        converting = "G2C"
+        print("Converting", inImg.size, "full-resolution grayscale image to RGB-encoded grays.")
+
+    grayPixels = inImg.load()
+    outImg = Image.new("RGB", (int(inImg.width/3), inImg.height))
+    rgbPixels = outImg.load() # Create pixel object for converted output
+    for x in range(outImg.width): # Loop over the output columns
+            inX = x * 3 # The input X-dimension is 3x the output rgb X-dimension.
+            for y in range(outImg.height):  # Loop over the rows
+                # Build a new rgb-encoded pixel from the next three gray pixels
+                newPixel = (grayPixels[inX, y],
+                        grayPixels[inX+1, y],
+                        grayPixels[inX+2, y])
+                rgbPixels[x, y] = newPixel # Assign the rgb-encoded pixel
+    
+    outImg.save(file)
 
 if yes_or_no("Do RGB Conversion?"):
     # RGB Conversion
@@ -66,10 +93,11 @@ if yes_or_no("Do RGB Conversion?"):
             outfile = "./"+tempdir+"/"+f
             infile = "./"+tempdir+"/"+f
             print("RGB-Conversion for image: " + infile + ". Output to: " + outfile)
-            os.system("python monoMSLAConvert.py " + infile + " " + outfile)
+            #os.system("python monoMSLAConvert.py " + infile + " " + outfile)
+            rgb_convert(infile)
      
 
-# ZIP erstellen
+# Create zip archive
 
 zip_file = zipfile.ZipFile(changename+".zip", 'w')
 for f in os.listdir(tempdir):
